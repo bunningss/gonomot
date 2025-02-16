4bc06ee5b89285363438e6ec339f562f391a35d1
tayef mahmud <mahmudovi183@gmail.com>
2025-02-16 16:51:06 +0600

    fix: add new poll route

--- src/app/api/polls/route.ts
+++ src/app/api/polls/route.ts
@@ -1,27 +1,45 @@
+import User from "@/lib/models/User";
+import mongoose from "mongoose";
 import Poll from "@/lib/models/Poll";
 import { connectDb } from "@/lib/db/connect-db";
 import { verifyToken } from "@/utils/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const session = await mongoose.startSession();
   try {
     await connectDb();
-    await verifyToken(request, "create:poll");
+    session.startTransaction();
+    const { id } = await verifyToken(request, "create:poll");
 
     const body = await request.json();
 
     const newPoll = await new Poll(body);
 
-    await newPoll.save();
+    await User.findByIdAndUpdate(
+      id,
+      {
+        $push: { polls: newPoll._id },
+      },
+      {
+        new: true,
+        session,
+      }
+    );
+
+    await newPoll.save({ session });
 
     return NextResponse.json(
       { msg: "Poll created successfully." },
       { status: 200 }
     );
   } catch (error) {
+    await session.abortTransaction();
     return NextResponse.json(
       { msg: (error as Error).message },
       { status: 400 }
     );
+  } finally {
+    await session.endSession();
   }
 }
